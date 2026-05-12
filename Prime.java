981c8c03da3126c3d5fceac80a4a4e31d7888f59
Vinit Yadav <vinityadav7307@gmail.com>
2026-05-13 02:04:22 +0530

    Prime number logic

--- Prime.java
+++ Prime.java
@@ -1,10 +1,27 @@
 import java.util.*;
 public class Prime {
-    public static void main(string[] args){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number:");5
         int num = sc.nextInt();
-        Syatem.out.println("Enter Number");
 
+        if(num==2){
+            System.out.println("Num is Prime");
+        }else{
+
+        boolean isPrime = true;
+        for(int i=2; i<=num-1; i++){
+            if(num%i == 0) {
+                isPrime = false;
+            }
+        }
+        if(isPrime == true){
+            System.out.println("num is Prime");
+        }else{
+            System.out.println("Num is not Prime");
+        }
+    }
+    sc.close();
 
     }
 }
