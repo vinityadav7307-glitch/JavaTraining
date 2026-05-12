b68422444496be32f64e76bfc5fc5523cb03e1d4
echo_04 <vinit.yadav7307@gmail.com>
2026-05-13 02:14:47 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Prime {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number:");5
+        System.out.println("Enter number:");
         int num = sc.nextInt();
 
         if(num==2){
