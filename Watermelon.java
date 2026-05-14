61c2f1b14087a3369fdc069f173ecd6384d499b3
Vinit Yadav <vinityadav7307@gmail.com>
2026-05-14 22:56:58 +0530

    Day 4

---
+++ Watermelon.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Watermelon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int W = sc.nextInt();
+
+        if(W<=2){
+            System.out.println("No");
+        }else if(W%2==0){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
