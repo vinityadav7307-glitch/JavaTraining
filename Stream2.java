c5269fa2d7392cf5ed0f756517bb256e030b13b7
Vinit Yadav <vinit.yadav7307@gmail.com>
2026-06-05 10:45:54 +0530

    Practice

--- Stream2.java
+++ Stream2.java
@@ -1,13 +1,14 @@
 import java.util.List;
 import java.util.stream.Collectors;
+
 public class Stream2 {
     public static void main(String[] args) {
-        List<String> rawData = List.of("apple","banana","kiwi","grapes","avacado");
+        List<String> rawData = List.of("apple", "banana", "kiwi", "grapes", "avacado");
         List<String> onlyAwords = rawData.stream()
 
-        .filter(word -> word.startsWith("a"))
-        .collect(Collectors.toList());
-        System.out.println("Original list"+rawData);
+                .filter(word -> word.startsWith("a"))
+                .collect(Collectors.toList());
+        System.out.println("Original list" + rawData);
         System.out.println("new list" + onlyAwords);
     }
 }
