7618f08bb621db6703bbd2d305e6a89522bf856e
Vinit Yadav <vinit.yadav7307@gmail.com>
2026-06-09 10:39:27 +0530

    Practice

---
+++ String1.java
@@ -0,0 +1,8 @@
+public class String1 {
+    public static void main(String[] args){
+        String name = "Vinit";
+       // String name = "Vineet";  //(Already define error)
+        name = "Vineet";
+        System.out.println(name);
+    }
+}
