99691c2858e10c2c62e6fe141fd8d63fb96acfce
Vinit Yadav <vinit.yadav7307@gmail.com>
2026-06-09 11:21:58 +0530

    Practice

---
+++ String2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class String2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Word: ");
+        String str = sc.next();
+        System.out.println(str.length());
+        for (int i=0;i<str.length();i++){
+            char ch = Character.toLowerCase(str.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+                System.out.println(ch);
+           }   
+        }
+    }
+}
