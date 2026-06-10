e07bbae66bf56fa86f629fb5734ee8e28f7a69c5
Vinit Yadav <vinit.yadav7307@gmail.com>
2026-06-10 15:10:05 +0530

    Back Tracking

---
+++ RecursionBackTracking.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class RecursionBackTracking {
+
+    public static void helper(int i, int n, int arr[], ArrayList<Integer> temp) {
+
+        if (i >= n) {
+            System.out.println(temp);
+            return;
+        }
+
+        // Take
+        temp.add(arr[i]);
+        helper(i + 1, n, arr, temp);
+
+        // Backtrack
+        temp.remove(temp.size() - 1);
+
+        // Not Take
+        helper(i + 1, n, arr, temp);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4};
+        helper(0, arr.length, arr, new ArrayList<>());
+    }
+}
