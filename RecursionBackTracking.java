cf6b585440e262d0360ad2ff11736b63dc4e051d
Vinit Yadav <vinit.yadav7307@gmail.com>
2026-06-16 10:11:20 +0530

    Recursion

--- RecursionBackTracking.java
+++ RecursionBackTracking.java
@@ -21,7 +21,7 @@ public class RecursionBackTracking {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4};
+        int arr[] = {1, 2, 3};
         helper(0, arr.length, arr, new ArrayList<>());
     }
 }
